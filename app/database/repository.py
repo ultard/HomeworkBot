3397a6e2e09b0c0919c6696812de8fddb3aabea2
ultar <glebpozbnakov62@gmail.com>
2024-10-24 14:31:22 +0300

    feat: базовый репозиторий

--- app/database/repository.py
+++ app/database/repository.py
@@ -1,37 +1,47 @@
-from typing import Dict, Any
-
-from sqlalchemy import select, update
+from typing import Type, TypeVar, Generic, Dict, Any, Optional, List
+from sqlalchemy import select, update, delete
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
+from app.database.models import Base, User
 
-from app.database.models import User
+T = TypeVar("T", bound=Base)
 
-
-class UserRepository:
-    def __init__(self, session: AsyncSession):
+class BaseRepository(Generic[T]):
+    def __init__(self, model: Type[T], session: AsyncSession):
+        self.model = model
         self.session = session
 
-    async def set_user(self, user: User):
+    async def add(self, entity: T) -> None:
         try:
-            self.session.add(user)
+            self.session.add(entity)
             await self.session.commit()
         except IntegrityError:
             await self.session.rollback()
 
-    async def get_user(self, user_id: int, relations: list[str] = None) -> User | None:
-        query = select(User).where(user_id == User.id)
+    async def get(self, entity_id: int, relations: Optional[List[str]] = None) -> Optional[T]:
+        query = select(self.model).where(self.model.id == entity_id)
 
-        for relation in relations:
-            query = query.options(selectinload(getattr(User, relation)))
+        if relations:
+            for relation in relations:
+                query = query.options(selectinload(getattr(self.model, relation)))
 
         return await self.session.scalar(query)
 
-    async def update_user(self, user_id: int, data: Dict[str, Any]):
+    async def update(self, entity_id: int, data: Dict[str, Any]) -> None:
         await self.session.execute(
-            update(User)
-            .where(user_id == User.id)
+            update(self.model)
+            .where(self.model.id == entity_id)
             .values(**data)
         )
-
         await self.session.commit()
+
+    async def delete(self, entity_id: int) -> None:
+        await self.session.execute(
+            delete(self.model).where(self.model.id == entity_id)
+        )
+        await self.session.commit()
+
+class UserRepository(BaseRepository[User]):
+    def __init__(self, session: AsyncSession):
+        super().__init__(User, session)
